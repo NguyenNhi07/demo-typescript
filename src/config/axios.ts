ca8b096005a9ee083d3796b611dc8892028ae093
NguyenNhi07 <nguyennhi.hau2003@gmail.com>
2024-06-19 13:55:25 +0700

    add_user_logoutButton

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const customAxios = axios.create({
-  baseURL: 'https://ca89-118-70-190-141.ngrok-free.app',
+  baseURL: 'https://9f6e-118-70-190-141.ngrok-free.app/',
   headers: {
     'ngrok-skip-browser-warning': '69420',
     Authorization: `Bearer ${localStorage.getItem('token')}`,
